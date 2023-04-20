48f595c4614c254b335f1326a5cae5cc8c2eaccf
Jennifer Chan <jenchanws@gmail.com>
2023-04-20 02:09:10 -0400

    Fix version numbers

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val archives_base_name: String by project
 version = "$mod_version+mc$minecraft_version"
 group = maven_group
 
-val archives_version = "$mod_version-fabric+mc$minecraft_version"
+val archives_version = "$mod_version+mc$minecraft_version-fabric"
 
 repositories {
   mavenCentral()
